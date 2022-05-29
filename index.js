dc7e0af21f014b46eeed981a763d1f1241598d5f
salem z <salem.zerezghi2@gmail.com>
2022-05-28 21:00:38 -0400

    imported dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+// link to page creation
+const generateHTML = require('./src/generateHTML');
+
+// team profiles
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern'); 
+
+// node modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+// team array
+const teamArray = []; 
